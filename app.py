42fd776d35edc25238b38e4201cf736f9181e50b
gabrielsanthony <generation.gabriel@gmail.com>
2025-06-03 11:21:51 +1200

    Update app.py

--- app.py
+++ app.py
@@ -18,9 +18,9 @@ def get_base64_image(path):
 logo_base64 = get_base64_image("pillai_logo.png")
 
 st.markdown(f"""
-<div style='display: flex; align-items: center; justify-content: center; margin-bottom: 10px;'>
-    <img src='{logo_base64}' width='200' style='margin-right: 10px;'>
-    <h2 style='margin: 0; padding: 0;'>Your Smart Medicine Assistant</h2>
+<div style='text-align: center; margin-bottom: 10px;'>
+    <img src='{logo_base64}' width='200' style='margin-bottom: 5px;'>
+    <h2 style='margin-top: 0px;'>Your Smart Medicine Assistant</h2>
 </div>
 """, unsafe_allow_html=True)
 
